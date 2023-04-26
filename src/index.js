307e5a7942efe1b1812d5357899527be873acc54
Miss-Anastasiia <110167985+Miss-Anastasiia@users.noreply.github.com>
2023-04-26 22:58:11 +0300

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/test-task">
+  <BrowserRouter basename="/test-task-1">
     <App />
   </BrowserRouter>
 );
